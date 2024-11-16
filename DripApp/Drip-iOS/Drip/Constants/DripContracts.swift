b1912ba81f90824de7c55996c0e302df2112fac2
shane.chi <yuhsuan19@gmail.com>
2024-11-16 23:48:11 +0700

    feat: deploy

--- DripApp/Drip-iOS/Drip/Constants/DripContracts.swift
+++ DripApp/Drip-iOS/Drip/Constants/DripContracts.swift
@@ -7,9 +7,9 @@
 import Foundation
 
 enum DripContracts {
-    static let profile: String = "0xD9e11F95cFD11044b53Df6A25179610384092C79"
-    static let challenge: String = "0x7C1d1E7aB9b8d98c9c226B7A35AC1cEF767b1B5E"
-    static let challengeManager: String = "0x7932eB14b88882b4329413FDA9Fe0c06A44F75cD"
+    static let profile: String = "0xbaA1e7FA30539Bc29b28D0553d3c9b13bfD575CA"
+    static let challenge: String = "0xBef730bae27148884B7E853d0637a948F8700132"
+    static let challengeManager: String = "0xaE9fd9feaE497c733f53c304fAC2E67bC8148041"
 
     static let dripERC20Token: String = "0x235c36243BD73d65B530a469658FeF591daA2f45"
     static let dripERC20TokenSymbol: String = "DRP"
