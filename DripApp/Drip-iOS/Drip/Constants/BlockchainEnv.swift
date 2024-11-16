38b51c01a2131399988f2ac3862f400f0e149af3
shane.chi <yuhsuan19@gmail.com>
2024-11-16 23:58:28 +0700

    feat: chain env

--- DripApp/Drip-iOS/Drip/Constants/BlockchainEnv.swift
+++ DripApp/Drip-iOS/Drip/Constants/BlockchainEnv.swift
@@ -7,8 +7,15 @@
 import Foundation
 
 enum BlockchainEnv {
-    static let chainId: String = "84532"
-    static let rpcURL: String = "https://sepolia.base.org"
+    static let polygonRPC = "https://chainlist.org/chain/2442"
+    static let polygonChainID = "2442"
+    static let mantlePRC = "https://rpc.sepolia.mantle.xyz"
+    static let mantleChainID = "5003"
+    static let basePRC = "https://sepolia.base.org"
+    static let baseChainId = "84532"
+
+    static let chainId: String = BlockchainEnv.baseChainId
+    static let rpcURL: String = BlockchainEnv.basePRC
     static let nativeTokenSymbol: String = "ETH"
     static let nativeTokenIcon: String = "token-eth"
 }
